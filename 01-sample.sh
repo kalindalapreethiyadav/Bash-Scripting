b05027ebd2a242b5dae50e8ab57eda4936d10ff8
preethi k <kpreethiyadav579@gmail.com>
2022-07-13 22:22:32 +0530

    updating added cmds

--- 01-sample.sh
+++ 01-sample.sh
@@ -5,3 +5,15 @@ echo "hello word"
 # excape sequence can specified by  "-e " option
 
 echo -e "Hello\nGood\tMorning!"
+
+#alias gp="git pull" --can be used for created short cuts for the common commands that we freqently.
+
+#Adding forground(FG) color and background(BG) color
+#RGY 31-33(FG) 41-43(BG) & BMC 34-36 44-46(BG)
+
+echo -e "\e[31m testing color of Red FG \e[0m"
+echo -e "\e[31m testing color of green FG \e[0m"
+echo -e "\e[31m testing color of yellow FG \e[0m"
+echo -e "\e[31m testing color of blue FG \e[0m"
+echo -e "\e[31m testing color of magenta FG \e[0m"
+echo -e "\e[31m testing color of clay FG \e[0m"
