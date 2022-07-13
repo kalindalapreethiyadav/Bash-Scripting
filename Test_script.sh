5b2a8e0a68419467890d967852a3f2e6e033cfa7
preethi k <kpreethiyadav579@gmail.com>
2022-07-13 17:29:49 +0530

    updating added cmds

---
+++ Test_script.sh
@@ -0,0 +1,3 @@
+common_func() {
+    echo -e "[32m I am implicit functon in external script file [0m" 
+}
---
+++ common.sh
@@ -0,0 +1,6 @@
+#!/bin/bash/
+
+#calling a func from external script file
+source ./Test_script.sh
+
+common_func()
