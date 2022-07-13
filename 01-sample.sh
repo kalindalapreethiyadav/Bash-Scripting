1ea5de556e1a2e2581cde9c0b9b373dc964bb9d1
preethi k <kpreethiyadav579@gmail.com>
2022-07-13 22:27:49 +0530

    updating added cmds

--- 01-sample.sh
+++ 01-sample.sh
@@ -18,3 +18,12 @@ echo -e "\e[34m testing color of blue FG \e[0m"
 echo -e "\e[35m testing color of magenta FG \e[0m"
 echo -e "\e[36m testing color of clay FG \e[0m"
 
+#lets try both FG & BG color in one sentence
+
+echo -e "\e[31m;46m testing color of Red FG \e[0m"
+echo -e "\e[32m;45m testing color of green FG \e[0m"
+echo -e "\e[33m;44m testing color of yellow FG \e[0m"
+echo -e "\e[34m;43m testing color of blue FG \e[0m"
+echo -e "\e[35m;42m testing color of magenta FG \e[0m"
+echo -e "\e[36m;41m testing color of clay FG \e[0m"
+
