a2548478ea1e6df7a7b9ea0c5e0eef9cb50bf1ab
preethi k <kpreethiyadav579@gmail.com>
2022-07-14 07:57:27 +0530

    updating added cmds

--- Bash_special_var.sh
+++ Bash_special_var.sh
@@ -8,5 +8,6 @@ echo "fourth spec varaible 4 is $4" #40
 
 #Basic_special_var.sh 10 20 30 40
 
-echo $* #to count no of varibales in the current script use $*
+echo $* #to show all the varibales in the current script use $*
 
+echo $# #use $# to count no of varibles in current script
