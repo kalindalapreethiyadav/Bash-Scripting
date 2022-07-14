6fb22ce0729008eab4a8a852137536aaa106cbe4
preethi k <kpreethiyadav579@gmail.com>
2022-07-14 06:54:17 +0530

    updating added cmds

--- basic_bash2.sh
+++ basic_bash2.sh
@@ -4,8 +4,18 @@
 
 a=10 # 10 ia also string
 b=abc 
-c=/path/basic/
-
+c=/path/basic/  #i also acts as a string
 echo $a 
 echo $b
 echo $c
+
+if we are not delcaring the variable for example
+
+rm -rf /data_path/$data_file_path #if we didn't specificy the data_file_path entire data in dat_path will be deleted.
+
+d = "/Bash-Scripting/"
+echo $d
+cd $d 
+mkdir folder1
+cd folder1
+pwd
