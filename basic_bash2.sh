97f72015d3a687587645f3d36630edeb22f3a83b
preethi k <kpreethiyadav579@gmail.com>
2022-07-14 07:30:47 +0530

    updating added cmds

--- basic_bash2.sh
+++ basic_bash2.sh
@@ -8,6 +8,7 @@ c=/path/basic/  #i also acts as a string
 echo $a 
 echo $b
 echo ${b}
+echo "hello ${b} checking inside"
 echo $c
 #if we are not delcaring the variable for example
 #rm -rf /data/${data_dir} #if we didn't specificy the "/data/" whole dir will be deleted.
