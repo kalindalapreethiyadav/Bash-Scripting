9392ece6bcc0769c663f6c7524a33082241426a8
preethi k <kpreethiyadav579@gmail.com>
2022-07-14 22:49:33 +0530

    updating read file

--- basic_read.sh
+++ basic_read.sh
@@ -6,7 +6,7 @@ echo -e "printing the value that read : $Name"
 
 stat()
     {
-        echo -e "\e[32;42m todays data is $(date +%F) \e[0m"
+        echo -e "\e[32;41m todays data is $(date +%F) \e[0m"
         echo -e "present no of users logined today $(date +%F) is : $(who) or $(w)\n" 
         echo "$(uptime| awk -F : {print $2F})"
         sam
