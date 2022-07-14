926f2168af15680cbd11f73ee818f50f838f09ee
preethi k <kpreethiyadav579@gmail.com>
2022-07-14 07:52:26 +0530

    updating added cmds

--- Bash_special_var.sh
+++ Bash_special_var.sh
@@ -1,6 +1,9 @@
 #lets see declaring varaibes at command prompt end
 
-echo "first spec varaible 0 is $0"
-echo "second spec varaible 1 is $1"
-echo "third spec varaible 2 is $2"
-echo "fourth spec varaible 3 is $3"
+echo "prining file name is $0" #Basic_special_var.sh
+echo "first varaible argument 1 is $1" #10
+echo "second varaible argument 2 is $2" #20
+echo "third spec varaible 3 is $3" #30
+echo "fourth spec varaible 4 is $4" #40
+
+#Basic_special_var.sh 10 20 30 40
