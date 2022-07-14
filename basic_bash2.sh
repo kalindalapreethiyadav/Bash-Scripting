e0b281042dedd9cf4ee2ffc9c75b3914299622c0
preethi k <kpreethiyadav579@gmail.com>
2022-07-14 07:04:18 +0530

    updating added cmds

--- basic_bash2.sh
+++ basic_bash2.sh
@@ -7,11 +7,10 @@ b=abc
 c=/path/basic/  #i also acts as a string
 echo $a 
 echo $b
+echo ${b}
 echo $c
-
 #if we are not delcaring the variable for example
 #rm -rf /data/${data_dir} #if we didn't specificy the "/data/" whole dir will be deleted.
 #${b} & $b both are same
-${b}
-e = "/Bash-Scripting"
-echo $e "Hello I am in $e path"
+
+echo $c "Hello I am in $c path"
