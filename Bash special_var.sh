777343a0f6a470f145c87b8202bd5604fb38aeff
preethi k <kpreethiyadav579@gmail.com>
2022-07-14 07:47:02 +0530

    updating added cmds

---
+++ Bash special_var.sh
@@ -0,0 +1,6 @@
+#lets see declaring varaibes at command prompt end
+
+echo "first spec varaible 0 is $0"
+echo "second spec varaible 1 is $1"
+echo "third spec varaible 2 is $2"
+echo "fourth spec varaible 3 is $3"
