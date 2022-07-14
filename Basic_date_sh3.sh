8283dd35653fe6d174190f444b4da46fce70af94
preethi k <kpreethiyadav579@gmail.com>
2022-07-14 07:34:40 +0530

    updating added cmds

--- Basic_date_sh3.sh
+++ Basic_date_sh3.sh
@@ -2,6 +2,8 @@
 
 Date_cmd=$(date +%F)
 echo "printing teh date $Date_cmd"
+d=$(date +%F)
+echo "printing $(date +F)"
 echo -e "Lets print commands inside echo string $(who|wc -l)\n"
 echo -e "pwd ls of files $(pwd|ls -lrt)\n"
 echo  "$(pwd)"
