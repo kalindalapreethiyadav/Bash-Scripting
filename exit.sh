84395caf21823a71584cd8bd5e4eb055dbf44605
preethi k <kpreethiyadav579@gmail.com>
2022-07-15 20:03:22 +0530

    updating read file

--- exit.sh
+++ exit.sh
@@ -2,7 +2,10 @@
 
 #Exit code 0 to 255
 echo "Hello"
-exit  0 # for  scussfull excution $? prints 0
+# Example we use exit 0(succesfull) or exit 1-255 code(error)
+#in specfic func or anywhere in code we specify if u want to exit so that 
+#we can identify where the error is with the code error 
+exit 0 # for  scussfull excution $? prints 0
 exit 1 # we can use from 1 to 255 code number so that incidates error $? prints 1 as error
 
 $?
