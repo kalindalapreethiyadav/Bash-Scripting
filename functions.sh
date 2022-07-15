066c4d0e9d2da6063d1c93b34e1ae9a94c2907da
preethi k <kpreethiyadav579@gmail.com>
2022-07-15 21:32:32 +0530

    updating read file

---
+++ functions.sh
@@ -0,0 +1,35 @@
+#!/bin/bash/
+#operators on numbers
+# -ne -eq -gt -lt -le
+
+#conditions
+<<comment
+1)if
+2)else if
+3)elif 
+comment
+
+Action=$1
+
+#if statements
+
+if["$Action" = "start"]; then
+    echo "starting the statmts"
+fi
+
+
+if["$Action" = "start"]; then
+    echo "starting the statmts"
+else
+echo "stop else goes"
+fi
+
+if["$Action" = "start"]; then
+    echo "stopping the statmts"
+elif ["$Action" = "stop"]
+    echo "reboot else goes"
+elif ["$Action" = "restart"]
+    echo "restart"
+else
+    echo "No action"
+fi
