fbed1f81e75ca3c319b963e8d9f413b119ce1dcc
preethi k <kpreethiyadav579@gmail.com>
2022-07-15 19:50:15 +0530

    updating read file

--- exit.sh
+++ exit.sh
@@ -2,6 +2,8 @@
 
 #Exit code 0 to 255
 echo "Hello"
-exit 1
+exit  0 # for  scussfull excution $? prints 0
+exit 1 # we can use from 1 to 255 code number so that incidates error $? prints 1 as error
+
 $?
 echo "Hi"
