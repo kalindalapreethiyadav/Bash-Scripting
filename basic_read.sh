7b625d52e590f35a44f0c8394397383bfe3673f9
preethi k <kpreethiyadav579@gmail.com>
2022-07-15 13:08:18 +0530

    updating read file

--- basic_read.sh
+++ basic_read.sh
@@ -2,18 +2,20 @@
 
 read -p "reading value from user :" Name
 echo -e "printing the value that read : $Name"
-lod_avg="$(uptime|awk -F , '{print $3F}'|awk -F : '{print $NF}')"
+LOADAVG="$(uptime|awk -F , '{print $3F}'|awk -F : '{print $NF}')"
 
 stat()
     {
         echo -e "\e[32;41m todays data is $(date +%F) \e[0m"
-        echo -e "present no of users logined today $(date +%F) is : $(who|wc -l)"
-        echo -e "last uptime no of days $(uptime|awk -F , '{print $1F}'| awk -F ' ' '{print $3,$4}')"
-        echo -e "$(uptime| awk -F : '{print $2F}')"
+        echo "present no of users logined today $(date +%F) is : $(who|wc -l)"
+        echo "last uptime no of days $(uptime|awk -F , '{print $1F}'| awk -F ' ' '{print $3,$4}')"
+        echo "$(uptime| awk -F : '{print $2F}')"
         sam
-        echo "I am main func"
+        sleep 5
+        echo -e "I am main func\n"
+        sleep 2
         echo "$(uptime)"
-        echo -e "load avg for 1 minute : $(lod_avg)"
+        echo "$LOADAVG"
     }
 
     sam()
